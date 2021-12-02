12f3fc4212493e25d33865304203d05506c25dd7
Colin Kinloch <colin@kinlo.ch>
2021-12-02 22:17:13 +0000

    Initial work on new widget

---
+++ app/src/main/java/ch/kinlo/timesheetdx/TimesheetAppWidgetProviderK.kt
@@ -0,0 +1,74 @@
+package ch.kinlo.timesheetdx
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+
+import android.app.PendingIntent
+
+import android.os.Build
+
+import android.content.Context
+import android.content.Intent
+
+import android.graphics.Canvas;
+import android.graphics.Bitmap;
+
+import android.widget.RemoteViews;
+
+import android.util.Log
+
+import androidx.core.content.ContextCompat
+
+class TimesheetAppWidgetProviderK : AppWidgetProvider() {
+  override fun onUpdate(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetIds: IntArray,
+  ) {
+  appWidgetIds.forEach { appWidgetId ->
+  	Log.v("YERBB", "goo");
+
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            /* context = */ context,
+            /* requestCode = */  0,
+            /* intent = */ Intent(context, TimesheetActivity::class.java),
+            /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an on-click listener
+            // to the button.
+            val views: RemoteViews = RemoteViews(
+                    context.packageName,
+                    R.layout.app_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.select_task, pendingIntent)
+            }
+            
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+              views.setImageViewResource(R.id.prev_task, R.drawable.baseline_expand_less_36);
+              views.setImageViewResource(R.id.next_task, R.drawable.baseline_expand_more_36);
+            } else {
+              val bum = listOf(
+                  Pair(R.id.prev_task, R.drawable.baseline_expand_less_36),
+                  Pair(R.id.next_task, R.drawable.baseline_expand_more_36),
+                )
+              for (p in bum) {
+                var d = ContextCompat.getDrawable(context, p.first)!!
+                var b = Bitmap.createBitmap(d.getIntrinsicWidth(),
+                                            d.getIntrinsicHeight(),
+                                               Bitmap.Config.ARGB_8888)
+                var c = Canvas(b)
+                d.setBounds(0, 0, c.getWidth(), c.getHeight())
+                d.draw(c)
+                views.setImageViewBitmap(p.second, b)
+              }
+            }
+
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+            }
+
+  }
+}
